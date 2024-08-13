cbab45f609ccdcfe66adda8a17c83d092ae746a4
MajedBenAmara <majed.benamara4@gmail.com>
2024-08-13 17:52:37 +0100

    Adding SFX + music, completing imp and disapp plat

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private SoundSO _music;
+
+    private AudioSource _audioSource;
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = _music.Clips[0];
+        _audioSource.volume = _music.Volume;
+        _audioSource.Play();
+    }
+    private void Update()
+    {
+        _audioSource.volume = _music.Volume;
+    }
+
+    public void ChangeMusic(int index)
+    {
+        _audioSource.clip = _music.Clips[index];
+        _audioSource.Play();
+    }
+}
--- Assets/Scripts/Objects/HealPoint.cs
+++ Assets/Scripts/Objects/HealPoint.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealingPoint : MonoBehaviour
+public class HealPoint : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) {
             PlayerManager.Instance.ChangeHp(0, false, true);
+            SoundManager.Instance.PlayPlayerSfx("Holy Heal");
         }
     }
 }
